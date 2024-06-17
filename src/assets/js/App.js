4bf017cef06ae594dd59483fa0b95862cae55c41
VaibhavVe <vaibhavverma0620@gmail.com>
2024-06-17 22:49:58 +0530

    Change

---
+++ src/assets/js/App.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Aos from 'aos'
+import "../style/App.css";
+import Header from "../components/Header/Header";
+import Home from "../components/DSG/Home";
+import Exercises from "../components/DSG/Exercises";
+import Trainer from "../components/DSG/Trainer";
+import Pricing from "../components/DSG/Pricing";
+import Contact from "../components/DSG/Contact";
+import Footer from "../components/DSG/Footer";
+import Wlos from "../components/DSG/Wlos";
+
+function App() {
+
+  useEffect(() =>{
+    Aos.init();
+  },[])
+
+  return <>
+    <Header />
+    <Home />
+    <Exercises />
+    <Trainer />
+    <Pricing />
+    <Contact />
+    <Wlos />
+    <Footer />
+  </>
+}
+
+export default App
